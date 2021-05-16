365fd337a53277c6e39ce8ea01a29a07c9a89529
Raj Shah <rajs55996@gmail.com>
2021-05-16 17:40:06 +0530

    Dynamic cart added

--- User.py
+++ User.py
@@ -1,12 +1,14 @@
-from flask import Flask , render_template , request
+from flask import Flask , render_template , request ,session
 import mysql.connector
+import json
+import simplejson
 
 app=Flask(__name__)
-
+app.secret_key="Raj Project"
 @app.route('/')
 def signUp():
     return render_template("Login.html")
-
+    
 @app.route('/result',methods=['POST','GET'])
 def login():
     mydb=mysql.connector.connect(
@@ -27,10 +29,6 @@ def login():
         mydb.commit()
         mycursor.close()
     return render_template("Login.html")
-
-
-
-
 @app.route('/display',methods=['POST','GET'])
 def display():
     mydb = mysql.connector.connect(
@@ -43,26 +41,86 @@ def display():
     if (request.method == 'POST'):
         Signin = request.form
         email = Signin['Lemail']
+        session['email']=email
         password = Signin['Lpassword']
         mycursor.execute("select * from registration where email='" + email + "' and password='" + password + "'")
         r = mycursor.fetchall()
         count = mycursor.rowcount
         if (count == 1):
+            print(session['email'])
             return render_template("Grocery1.html")
         else:
             return render_template("Login.html")
+        
     mydb.commit()
     mycursor.close()
 @app.route('/Grocery1')
 def grocery():
-    return render_template("Grocery1.html")
+    if "email" in session:
+        return render_template("Grocery1.html")
+    return render_template("Login.html")
 @app.route('/Fruits')
 def fruits():
-    return render_template("Fruits.html")
+    if "email" in session:
+        return render_template("Fruits.html")
+    return render_template("Login.html")
 @app.route('/Vegetables')
 def vegetables():
-    return render_template("Vegetables.html")
+    if "email" in session:
+        return render_template("Vegetables.html")
+    return render_template("Login.html")
 @app.route('/Beverages')
 def beverages():
-    return render_template("Beverages.html")
+    if "email" in session:
+        return render_template("Beverages.html")
+    return render_template("Login.html")
+@app.route('/Snacks')
+def snacks():
+    if "email" in session:
+        return render_template("Snacks.html")
+    return render_template("Login.html")
+
+import itertools
+@app.route('/Cart')
+def cart():
+    if "email" in session:
+        mydb=mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="grocery_user"
+        )
+        mycursor=mydb.cursor()
+        mycursor.execute("select * from carts")
+        data = mycursor.fetchall()
+        r=mycursor.rowcount
+        datas={}
+        datas['info']=data
+        return render_template("newCart.html",datas=datas)
+        mycursor.close()
+    return render_template("Login.html")
+
+#####################
+@app.route('/insert',methods=["POST","GET"])
+def insert():
+    mydb=mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="grocery_user"
+    )
+    
+    mycursor=mydb.cursor()
+    if(request.method=='POST'):
+        data=request.get_json("text")
+        email=session["email"]
+        title=data['title']
+        price=int(data['price'])*int(data['quan'])
+        imgsrc=data['imgsrc']
+        quan=data['quan']
+        mycursor.execute("insert into carts(email,title,price,imgsrc,quantity)values(%s,%s,%s,%s,%s)",(email,title,price,imgsrc,quan))
+    mydb.commit()
+    mycursor.close()
+    return "hascjs"
+
 app.run(debug=True)
---
+++ static/JS/cartjs.js
@@ -0,0 +1,114 @@
+let notes = localStorage.getItem("notes");
+
+if (notes == null) {
+  notesObj = [];
+} else {
+  notesObj = JSON.parse(notes);
+}
+
+let html = "";
+let a = 0;
+notesObj.forEach(function (element, index) {
+  a = a + element.price * element.quan;
+  html =
+    html +
+    `<div class="container">
+      <table>
+          <thead>
+              <tr>
+              <th class="y1">Image</th>
+              <th class="y2">Title</th>
+              <th class="y3">Quantity</th>
+              <th class="y4">Price</th>
+              <th class="y5">Option</th>
+          </tr>
+          </thead>
+          <tbody>
+              <tr>
+              <td class="y1"><img src="${element.imgsrc}"></td>
+              <td class="y2">${element.title}</td>
+              <td class="y3">${element.quan}</td>
+              <td class="y4">${element.quan*element.price}Rs</td>
+              <td><button class="deleteitembutton" id="${index}"  onclick="deleteNote(this.id)" >Delete</button></td>
+          </tr>
+          </tbody>
+      </table>
+  </div>`
+
+;
+});
+a=`<div class="cartfruitmr">Total amount is ${a} Rs</div>`;
+let flex = document.getElementById("flex");
+let flexs = document.getElementById("flexs");
+try{
+if (notesObj.length != 0) {
+  flex.innerHTML = html;
+  flexs.innerHTML=a;
+} else {
+  flex.innerHTML = "Nothing to show! Please order something";
+}
+}
+catch(e)
+{
+  console.log(e);
+}
+
+function deleteNote(index) {
+  //   console.log("I am deleting", index);
+
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+
+  notesObj.splice(index, 1);
+  console.log(notesObj);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  localStorage.getItem("notes");
+
+  let html = "";
+  let a = 0;
+  notesObj.forEach(function (element, index) {
+    a = a + element.price * element.quan;
+    html =
+      html +
+      `<div class="container">
+      <table>
+          <thead>
+              <tr>
+              <th class="y1">Image</th>
+              <th class="y2">Title</th>
+              <th class="y3">Quantity</th>
+              <th class="y4">Price</th>
+              <th class="y5">Option</th>
+          </tr>
+          </thead>
+          <tbody>
+              <tr>
+              <td class="y1"><img src="${element.imgsrc}"></td>
+              <td class="y2">${element.title}</td>
+              <td class="y3">${element.quan}</td>
+              <td class="y4">${element.quan*element.price}Rs</td>
+              <td><button class="deleteitembutton" id="${index}"  onclick="deleteNote(this.id)" >Delete</button></td>
+          </tr>
+          </tbody>
+      </table>
+  </div>`
+
+;
+  });
+  a=`<div class="cartfruitmr">Total amount is ${a} Rs</div>`;
+  let flex = document.getElementById("flex");
+  let flexs = document.getElementById("flexs");
+  if (notesObj.length != 0) {
+    flex.innerHTML = html;
+    flexs.innerHTML=a;
+  } else {
+    flex.innerHTML ="Nothing to show! Use Add a Note section above to add notes.";
+      flexs.innerHTML="Total amount is 0";
+  }
+}
+
+
